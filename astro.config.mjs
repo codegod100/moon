0506b7e76ab3f760c8b0bd0b2405503c7d5e8aa6
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2024-12-29 01:06:34 -0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import deno from "@deno/astro-adapter";
+import preact from '@astrojs/preact';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact(), tailwind()],
+  adapter: deno()
+});

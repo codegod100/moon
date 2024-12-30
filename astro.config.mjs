4cd32dee11421c392f7f000c37a841e9bd2c86e2
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2024-12-29 16:15:41 -0800

    feat: add state management and enhance user interaction in pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import deno from "@deno/astro-adapter";
-import preact from '@astrojs/preact';
+import preact from "@astrojs/preact";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()],
-  adapter: deno()
+  integrations: [
+    preact(),
+    tailwind(),
+    // sentry(),
+    // spotlightjs()
+  ],
+  adapter: deno(),
 });

508bb700406666560dcd9845d0733c37338bfb71
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2024-12-29 16:45:33 -0800

    sentry no

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,12 @@ import preact from "@astrojs/preact";
 
 import tailwind from "@astrojs/tailwind";
 
-import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     preact(),
     tailwind(),
-    // sentry(),
-    // spotlightjs()
   ],
   adapter: deno(),
 });

742efaf1f48c6590beb3295bde4b8686f86cd750
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2024-12-29 22:28:56 -0800

    prose is the word

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,11 @@ import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 
 
+import mdx from "@astrojs/mdx";
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    preact(),
-    tailwind(),
-  ],
+  integrations: [preact(), tailwind(), mdx()],
   adapter: deno(),
 });

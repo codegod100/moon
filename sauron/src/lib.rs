ef8bd95b4266a614ba2bc7b12f823350d8f4dcf6
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2025-01-01 19:58:23 -0800

    auto build

--- sauron/src/lib.rs
+++ sauron/src/lib.rs
@@ -30,21 +30,23 @@ impl Application for App {
     node! {
         <main>
         <input type="button"
-        class="inline-block px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition-colors shadow-md"
-        value="+"
-        on_click=|_| {
-            Msg::Increment
-        }
-            />
-            <button class="count ml-2 mr-2" on_click=|_|{
-                Msg::Reset} >{text(self.count)}</button>
-            <input type="button"
+            class="inline-block px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition-colors shadow-md"
+            value="+"
+            on_click=|_| {
+                Msg::Increment
+            }
+        />
+        <button class="count ml-2 mr-2" on_click=|_|{
+            Msg::Reset} >
+            {text(self.count)}
+        </button>
+        <input type="button"
             class="inline-block px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition-colors shadow-md"
             value="-"
             on_click=|_| {
                 Msg::Decrement
             }
-            />
+        />
         </main>
     }
   }

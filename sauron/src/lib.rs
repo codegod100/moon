4909d378615c193b1ecb2ca79c31dc541678acd0
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2025-01-01 16:53:55 -0800

    logging

--- sauron/src/lib.rs
+++ sauron/src/lib.rs
@@ -1,6 +1,8 @@
 use sauron::{
     html::text, html::units::px, jss, node, wasm_bindgen, Application, Cmd, Node, Program,
 };
+use log::Level;
+
 
 enum Msg {
     Increment,
@@ -25,41 +27,42 @@ impl Application for App {
     fn view(&self) -> Node<Msg> {
         node! {
             <main>
-                <input type="button"
-                class="inline-block px-4 py-2 bg-pink-500 text-white rounded-full hover:bg-pink-600 transition-colors shadow-md"
-                    value="+"
-                    on_click=|_| {
-                        Msg::Increment
-                    }
+            <input type="button"
+            class="inline-block px-4 py-2 bg-pink-500 text-white rounded-full hover:bg-pink-600 transition-colors shadow-md"
+            value="+"
+            on_click=|_| {
+                Msg::Increment
+            }
                 />
                 <button class="count" on_click=|_|{
                     Msg::Reset} >{text(self.count)}</button>
                 <input type="button"
                 class="inline-block px-4 py-2 bg-pink-500 text-white rounded-full hover:bg-pink-600 transition-colors shadow-md"
-                    value="-"
-                    on_click=|_| {
-                        Msg::Decrement
-                    }
+                value="-"
+                on_click=|_| {
+                    Msg::Decrement
+                }
                 />
             </main>
         }
     }
-
+    
     fn update(&mut self, msg: Msg) -> Cmd<Msg> {
         match msg {
             Msg::Increment => self.count += 1,
             Msg::Decrement => self.count -= 1,
             Msg::Reset => {
-                println!("resetting count");
+                log::info!("resetting count");
                 self.count = 1},
+            }
+            Cmd::none()
         }
-        Cmd::none()
+        
+        
     }
-
     
-}
-
-#[wasm_bindgen(start)]
-pub fn start() {
+    #[wasm_bindgen(start)]
+    pub fn start() {
+    console_log::init_with_level(Level::Debug);
     Program::mount_to_body(App::new());
 }

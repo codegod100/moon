7bf176de63141786026e1b7d529887cd35c03740
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2025-01-06 02:12:14 -0800

    one more tweak

--- src/pages/client-metadata.json.ts
+++ src/pages/client-metadata.json.ts
@@ -1,7 +1,6 @@
 import { metadata } from "../store/atproto.ts";
 export const prerender = false;
 export async function GET({ params, request }) {
-  console.log("wtf");
   return new Response(JSON.stringify(await metadata()), {
     headers: { "Content-Type": "application/json" },
   });

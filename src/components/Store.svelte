f80c72a49407f901c5883845af5688b8f69ea28b
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2025-01-06 02:08:09 -0800

    dynamic load

--- src/components/Store.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import { user, t_string } from "../store/user.ts";
-</script>
-
-<div class="text-xl">
-  <img src={$user.avatar} class="max-w-40" /> Hello my name is {$user.name}, I
-  am {$user.age} years old.
-</div>
--- src/pages/client-metadata.json.ts
+++ src/pages/client-metadata.json.ts
@@ -1,6 +1,8 @@
 import { metadata } from "../store/atproto.ts";
+export const prerender = false;
 export async function GET({ params, request }) {
-  return new Response(JSON.stringify(metadata), {
+  console.log("wtf");
+  return new Response(JSON.stringify(await metadata()), {
     headers: { "Content-Type": "application/json" },
   });
 }
